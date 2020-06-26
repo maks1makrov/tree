2bca92f2549b4ff81c6c099a920c2585508af313
Maks <maks1makrov@gmail.com>
2020-06-26 09:31:31 +0300

    manager for tree

---
+++ example2.py
@@ -0,0 +1,55 @@
+class Manager:
+    def __init__(self, root):
+        self.root = root
+
+    def add(self, new_node):
+        cursore = self.root
+        while True:
+            if new_node.mark > cursore.mark:
+                if cursore.right is not None:
+                    cursore = cursore.right
+                else:
+                    cursore.right = new_node
+                    new_node.parent = cursore
+                    break
+            else:
+                if cursore.left is not None:
+                    cursore = cursore.left
+                else:
+                    cursore.left = new_node
+                    new_node.parent = cursore
+                    break
+
+    def find(self, mark):
+        cursore = self.root
+        while True:
+            if mark == cursore.mark:
+                return cursore
+            if mark > cursore.mark:
+                if cursore.right is not None:
+                    cursore = cursore.right
+                else:
+                    return None
+            else:
+                if cursore.left is not None:
+                    cursore = cursore.left
+                else:
+                    return None
+
+    def find_all(self, mark):
+        cursore = self.root
+        result = []
+        while True:
+            if mark == cursore.mark:
+                result.append(cursore.name)
+            if mark > cursore.mark:
+                if cursore.right is not None:
+                    cursore = cursore.right
+                else:
+                    return result
+            else:
+                if cursore.left is not None:
+                    cursore = cursore.left
+                else:
+                    return result
+
