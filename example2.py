cfa00a3daaa7af741985110ffb4007856b277b3a
Maks <maks1makrov@gmail.com>
2020-06-26 09:47:07 +0300

    +delete

--- example2.py
+++ example2.py
@@ -53,6 +53,54 @@ class Manager:
                 else:
                     return result
 
+    def delete(self, mark):
+        cursore = self.root
+        while True:
+            if mark == cursore.mark:
+                if cursore.left and cursore.right:
+                    new = cursore.right
+                    while True:
+                        if new.left:
+                            new = new.left
+                        else:
+                            break
+                    cursore.name, cursore.mark = new.name, new.mark
+                    if new.right:
+                        new_next = new.right
+                        new.name, new.mark, new.left, new.right = new_next.name, new_next.mark, new_next.left, new_next.right
+                        del(new_next)
+                    else:
+                        new_next = new.parent
+                        new_next.left = None
+                        del(new)
+                elif cursore.left or cursore.right:
+                    if cursore.left:
+                        new = cursore.left
+                        cursore.name, cursore.mark, cursore.right, cursore.left = new.name, new.mark, new.right, new.left
+                        del(new)
+                    else:
+                        new = cursore.right
+                        cursore.name, cursore.mark, cursore.right, cursore.left = new.name, new.mark, new.right, new.left
+                        del(new)
+                else:
+                    new = cursore.parent
+                    if new.right == cursore:
+                        new.right = None
+                    else:
+                        new.left = None
+                    del(cursore)
+                return f"deleted of mark - {mark} is done "
+            if mark > cursore.mark:
+                if cursore.right is not None:
+                    cursore = cursore.right
+                else:
+                    return f"can not find mark - {mark}"
+            else:
+                if cursore.left is not None:
+                    cursore = cursore.left
+                else:
+                    return f"can not find mark - {mark}"
+
 
 class Node:
     def __init__(self, name, mark):
